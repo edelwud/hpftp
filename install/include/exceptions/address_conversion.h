9785748309b6f484b0c6f24798d1838700e7a8c4
edelwud <edelwud@icloud.com>
2021-08-05 16:02:30 +0300

    remove: binaries

--- install/include/exceptions/address_conversion.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-#include <exceptions/internal_exception.h>
-
-class AddressConversion : public InternalException {
-    const char *what() const noexcept override;
-};
--- install/include/exceptions/listen_exception.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <string>
-
-#include <exceptions/internal_exception.h>
-
-class ListenException : public InternalException {
-    const char *what() const noexcept override;
-};
